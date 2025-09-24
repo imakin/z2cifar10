51d1bed04c79525b7ee13fb1820c96d2357bace6
makin <makin.ugm@gmail.com>
2025-09-24 11:02:15 +0700

    update project.tcl and add test npy

--- hls_output/mc2hc6/project.tcl
+++ hls_output/mc2hc6/project.tcl
@@ -1,18 +1,3 @@
-# variable project_name
-# set project_name "mc2hc6c6"
-# variable backend
-# set backend "vitis"
-# variable part
-# set part "xc7z020clg400-1"
-# variable clock_period
-# set clock_period 5
-# variable clock_uncertainty
-# set clock_uncertainty 27%
-# variable version
-# set version "1.0.0"
-# variable maximum_size
-# set maximum_size 4096
-
 variable project_name
 set project_name "mc2hc6"
 variable backend
@@ -24,12 +9,12 @@ set clock_period 10
 variable version
 set version "1.0.0"
 
-# HLS build script for mc2hc6 with 64-bit AXIS wrapper top
+# HLS build script for $project_name with 64-bit AXIS wrapper top
 # Works on classic Vitis HLS CLI; for Vitis 2024/2025 unified, use v++ with --hls.pre_tcl to source this file.
 
 set src_dir "/home/makin/development/fpga/source/cnnmakin/makin_cifar/hls_output/$project_name/firmware"
 
-# membuat folder ./mc2hc6
+# membuat folder ./$project_name
 open_project $project_name
 set_top mc2h_axis64
 
@@ -41,19 +26,19 @@ add_files "$src_dir/${project_name}.cpp"
 # Testbench
 add_files -tb "$src_dir/tb_mc2h_axis64.cpp"
 
-# membuat folder ./mc2hc6/sol1, IP disitu
+# membuat folder ./$project_name/sol1, IP disitu
 open_solution sol1
 set_part $part
 create_clock -period $clock_period -name default
 
-# C-simulation (provide npy via working dir/file)
-catch { csim_design }
+# C-simulation (pass path to test image)
+catch { csim_design -argv "${src_dir}/../tb_data/X_test2.npy" }
 
 # Synthesis with parallel jobs
-csynth_design -jobs 6
+csynth_design
 
-# Optionally co-sim and export IP with parallel jobs
-# catch { cosim_design -rtl verilog -jobs 6 }
+# RTL co-sim, without waveform tracing (fastest accurate check)
+catch { cosim_design -rtl verilog -tool xsim -argv "${src_dir}/../tb_data/X_test2.npy" }
 export_design -format ip_catalog -ipname ${project_name}_axis64
 
 exit
